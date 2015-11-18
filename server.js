e0fbabc505c75a3e7a434ed13a18aca7b72f897b
AFlepp <antoineflepp@hotmail.fr>
2015-11-18 20:44:46 +0100

    On it !

--- server.js
+++ server.js
@@ -35,74 +35,85 @@ var wss = new WSServer({server: server});
  * When the connection with the websocket is closed.
  */
 wss.on("connection", function(ws){
-  ws.on("message", function(data){
-  
-    var msg = JSON.parse(data);
-    var dataToSend;
-  
-    /*
-     * When the websocket receive a message,
-     * do somethign depending of the "code" the client put
-     * into its message.
-     *
-     * It's sort of a home-made protocol for the game.
-     */
-
-    switch(msg.code){
-      case 1:
-        // NEW PLAYER
-        // WHEN A PLAYER CONNECTS TO THE SERVER
-        //
-        // Datas should be... well, nothing I guess. 
-        // The client will just say 'Hi'
-        //
-        // Treatement : 
-        // 1) UPDATE DATAS 
-        //  Give the player its server-side id
-        //  and send it to him
-        //  -> send player code 1
-        //  * Ex : ws.send(JSON.stringify({ -> JSON is important if you don't use broadcast
-        //          code: 1,
-        //          playerID: this.playerID -> Or whatever you decided
-        //                                      this player's ID is going
-        //                                      to be.
-        //         }))
-        // 2) BROADCAST NEW DATA 
-        //  Send to everyone that a new player entered the game and give 
-        //  them its position/color/ID/ whatever identifies him to others)
-        //  -> send broadcast code 2
-        break;
-      case 5:
-        // A PLAYER CHANGED HIS DIRECTION
-        //
-        // Should be something like the player ID/color/position
-        // And the new direction.
-        //
-        // 1) UDPATE DATA 
-        //  Change the internal state with the new 
-        //  direction for the right player.
-        // 2) BROADCAST PLAYER & NEW DIRECTION
-        //  Notify everyone about this player's change;
-        //  -> send broadcast code 5
-        break;
-      case 2:
-        // Put that just in case (joke). 
-        // Not sure we will use the 2 on the server side
-        break;
-      case 3:
-        // We should not be in need of this too, actually.
-        break;
-      case 4:
-        // This neither.
-        break;
-    }
-  });
-
-  ws.on("close", function(){
-    // WHEN A PLAYER DISCONNECTS
-  })
-
-
+	var nbMotos += 1;
+	var tabID = [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false]];
+	
+	while (nbMoto > 0){	
+	  ws.on("message", function(data){
+	    var msg = JSON.parse(data);
+	    var dataToSend;
+	  
+	    /*
+	     * When the websocket receive a message,
+	     * do something depending of the "code" the client put
+	     * into its message.
+	     *
+	     * It's sort of a home-made protocol for the game.
+	     */
+	
+	    switch(msg.code){
+	      case 1:   	 
+	    	 
+	    	tabID [0][1]= true;
+	    	this.playerID = tabID [0][0];
+	    	ws.send(JSON.stringify({
+	                      code: 1,
+	                      playerID: this.playerID
+	                     }))
+	        // NEW PLAYER
+	        // WHEN A PLAYER CONNECTS TO THE SERVER
+	        //
+	        // Datas should be... well, nothing I guess. 
+	        // The client will just say 'Hi'
+	        //
+	        // Treatement : 
+	        // 1) UPDATE DATAS 
+	        //  Give the player its server-side id
+	        //  and send it to him
+	        //  -> send player code 1
+	        //  * Ex : ws.send(JSON.stringify({ -> JSON is important if you don't use broadcast
+	        //          code: 1,
+	        //          playerID: this.playerID -> Or whatever you decided
+	        //                                      this player's ID is going
+	        //                                      to be.
+	        //         }))
+	        // 2) BROADCAST NEW DATA 
+	        //  Send to everyone that a new player entered the game and give 
+	        //  them its position/color/ID/ whatever identifies him to others)
+	        //  -> send broadcast code 2
+	        break;
+	      case 5:
+	        // A PLAYER CHANGED HIS DIRECTION
+	        //
+	        // Should be something like the player ID/color/position
+	        // And the new direction.
+	        //
+	        // 1) UDPATE DATA 
+	        //  Change the internal state with the new 
+	        //  direction for the right player.
+	        // 2) BROADCAST PLAYER & NEW DIRECTION
+	        //  Notify everyone about this player's change;
+	        //  -> send broadcast code 5
+	        break;
+	      case 2:
+	        // Put that just in case (joke). 
+	        // Not sure we will use the 2 on the server side
+	        break;
+	      case 3:
+	        // We should not be in need of this too, actually.
+	        break;
+	      case 4:
+	        // This neither.
+	        break;
+	    }
+	  });
+	  
+	  ws.on("close", function(){
+		    // WHEN A PLAYER DISCONNECTS
+			  
+			  nbMoto -= 1;
+	  })
+	}
 });
 
 // ------------------------------------------------------ Define WSS functions
